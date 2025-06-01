20e36e5df3d6c6f0d0a4847663a202ab6bbd2f0d
StcInc <STC.game.zone@gmail.com>
2025-06-01 18:02:22 +0300

    lil cleanup

--- main.cc
+++ main.cc
@@ -1,11 +1,12 @@
-// #include "raylib.h"
+// Raylib docs/cheatsheet
+// https://www.raylib.com/cheatsheet/cheatsheet.html
 
 #include <raylib.h>
-#include <stdlib.h>
 
 #define SCREEN_WIDTH (800)
 #define SCREEN_HEIGHT (450)
 
+#define FONT_SIZE 20
 #define TEXT "OMG! IT WORKS!"
 
 
@@ -19,15 +20,32 @@ int main() {
         ClearBackground(RAYWHITE);
 
         // do your drawing here
+
         DrawTriangle(
-            {SCREEN_WIDTH / 4, 3 * SCREEN_HEIGHT / 4},  // point a
+            {SCREEN_WIDTH / 4,     3 * SCREEN_HEIGHT / 4},  // point a
             {3 * SCREEN_WIDTH / 4, 3 * SCREEN_HEIGHT / 4},  // point b
-            {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4}, // point c
-            BLUE       //  color
+            {SCREEN_WIDTH / 2,     SCREEN_HEIGHT / 4    },  // point c
+            BLUE                                            //  color
         );
+
+
         // output some text
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), TEXT, 20, 1);
-        DrawText(TEXT, SCREEN_WIDTH / 2 - text_size.x / 2, SCREEN_HEIGHT / 2 + text_size.y / 2 + 10, 20, WHITE);
+
+        const Vector2 text_size = MeasureTextEx(
+            GetFontDefault(), // font
+            TEXT,             // text
+            FONT_SIZE,        // fontSize
+            1                 // spacing
+        );
+        const int x = SCREEN_WIDTH / 2 - text_size.x / 2;
+        const int y = SCREEN_HEIGHT / 2 + text_size.y / 2 + 10;
+        DrawText(
+            TEXT,      // text
+            x,         // posX
+            y,         // posY
+            FONT_SIZE, // fontSize
+            WHITE      // color
+        );
 
         EndDrawing();
     }
